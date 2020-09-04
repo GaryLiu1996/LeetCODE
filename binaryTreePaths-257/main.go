b9a379917bb92f0f669aaf0a6065bbacc3337cfb
dhc-liusiyuan <siyuan.liu@dhc.com.cn>
2020-09-04 10:53:05 +0800

    update 257

--- binaryTreePaths-257/main.go
+++ binaryTreePaths-257/main.go
@@ -1,1 +1,31 @@
-package binaryTreePaths_257
+package main
+
+import "strconv"
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+var paths []string
+
+func main(){
+}
+
+func binaryTreePaths(root *TreeNode) []string {
+	dfsTree(root,"")
+	return paths
+}
+func dfsTree(root *TreeNode,path string){
+	if root !=nil{
+		path += strconv.Itoa(root.Val)
+		if root.Left ==nil && root.Right ==nil{
+			paths = append(paths,path)
+		}else{
+			path+= "->"
+			dfsTree(root.Left,path)
+			dfsTree(root.Right,path)
+		}
+	}
+}
