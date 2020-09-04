875a82bb8521560c82c1df8b163777a1d4056738
dhc-liusiyuan <siyuan.liu@dhc.com.cn>
2020-09-04 11:00:03 +0800

    update 257

--- binaryTreePaths-257/main.go
+++ binaryTreePaths-257/main.go
@@ -1,31 +1,57 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 type TreeNode struct {
-	Val int
-	Left *TreeNode
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
 
 var paths []string
 
-func main(){
+func main() {
+	treeEx := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val:   4,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   5,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	}
+	fmt.Println("paths:", binaryTreePaths(treeEx))
 }
 
 func binaryTreePaths(root *TreeNode) []string {
-	dfsTree(root,"")
+	paths = []string{}
+	dfsTree(root, "")
 	return paths
 }
-func dfsTree(root *TreeNode,path string){
-	if root !=nil{
+func dfsTree(root *TreeNode, path string) {
+	if root != nil {
 		path += strconv.Itoa(root.Val)
-		if root.Left ==nil && root.Right ==nil{
-			paths = append(paths,path)
-		}else{
-			path+= "->"
-			dfsTree(root.Left,path)
-			dfsTree(root.Right,path)
+		if root.Left == nil && root.Right == nil {
+			paths = append(paths, path)
+		} else {
+			path += "->"
+			dfsTree(root.Left, path)
+			dfsTree(root.Right, path)
 		}
 	}
 }
