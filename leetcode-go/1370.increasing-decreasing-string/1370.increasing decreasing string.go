88b189b780c7e9d5152246cd1fc23dd4e12d5bdd
GaryLiu1996 <1099365856@qq.com>
2020-11-25 16:53:19 +0800

    update 1370.increasing-decreasing-string

---
+++ leetcode-go/1370.increasing-decreasing-string/1370.increasing decreasing string.go
@@ -0,0 +1,82 @@
+package _1370_increasing_decreasing_string
+
+import (
+	"fmt"
+	"reflect"
+)
+
+////自己的解法： 哈希表：效率低
+//执行用时：
+//12 ms
+//, 在所有 Go 提交中击败了
+//32.69%
+//的用户
+//内存消耗：
+//5 MB
+//, 在所有 Go 提交中击败了
+//23.08%
+//的用户
+//func sortString(s string) string {
+//	sortMap := make(map[byte]int, len(s))
+//	for _, value := range s {
+//		sortMap[byte(value)]++
+//	}
+//	arr := []byte{}
+//	for len(arr) < len(s) {
+//		for i := byte('a'); i < byte('z')+1; i++ {
+//			if sortMap[i] > 0 {
+//				arr = append(arr,i)
+//				sortMap[i]--
+//			}
+//		}
+//		for i := byte('z'); i > byte('a')-1; i-- {
+//			if sortMap[i] > 0 {
+//				arr = append(arr,i)
+//				sortMap[i]--
+//			}
+//		}
+//	}
+//	return string(arr)
+//}
+
+////官方答案：桶计数
+//执行用时：
+//4 ms
+//, 在所有 Go 提交中击败了
+//79.81%
+//的用户
+//内存消耗：
+//3 MB
+//, 在所有 Go 提交中击败了
+//91.35%
+//的用户
+func sortString(s string) string {
+	fmt.Println("AAAAA", 'a')                                                                 //AAAAA 97-------输出的是a的utf-8编码
+	cnt := ['z' + 1]int{}                                                                     //GO的字符有两种 byte(utf-8)、rune(int64)，这里是前者。
+	fmt.Println("BBBBB", reflect.TypeOf('z'+1), reflect.TypeOf(1), reflect.TypeOf(byte('z'))) //int32 int uint8
+	for _, ch := range s {
+		cnt[ch]++
+	}
+	n := len(s)
+	ans := make([]byte, 0, n)
+	for len(ans) < n {
+		for i := byte('a'); i <= 'z'; i++ {
+			if cnt[i] > 0 { //TODO:为什么这里KEY定义的是INT32而I是BYTE仍然没有报错？
+				ans = append(ans, i)
+				cnt[i]--
+			}
+		}
+		for i := byte('z'); i >= 'a'; i-- {
+			if cnt[i] > 0 {
+				ans = append(ans, i)
+				cnt[i]--
+			}
+		}
+	}
+	return string(ans)
+}
+
+//作者：LeetCode-Solution
+//链接：https://leetcode-cn.com/problems/increasing-decreasing-string/solution/shang-sheng-xia-jiang-zi-fu-chuan-by-leetcode-solu/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
---
+++ leetcode-go/1370.increasing-decreasing-string/1370.increasing decreasing string_test.go
@@ -0,0 +1,7 @@
+package _1370_increasing_decreasing_string
+
+import "testing"
+
+func Test_1370_increasing_decreasing_string(t *testing.T) {
+	sortString("leetcode")
+}
