cc3dd3834c427bab684fe56f1bfba1ba094da0a2
GaryLiu1996 <1099365856@qq.com>
2020-12-07 17:05:15 +0800

    update 0118.pascals-triangle

--- leetcode-go/0118.pascals-triangle/0118.go
+++ leetcode-go/0118.pascals-triangle/0118.go
@@ -7,10 +7,10 @@ func generate(numRows int) [][]int {
 	fmt.Println("ans", ans)       //[[] [] []]
 	fmt.Println("ans", ans[0])    //[]
 	for i := range ans {
-		ans[i] = make([]int, i+1)
-		ans[i][0] = 1
-		ans[i][i] = 1
-		for j := 1; j < i; j++ { //保证 i>=2时（第三层），才需要计算。
+		ans[i] = make([]int, i+1) //内层定义长度
+		ans[i][0] = 1             //首是1不需要加入循环
+		ans[i][i] = 1             //尾是1不需要加入循环
+		for j := 1; j < i; j++ {  //保证 i>=2时（第三层），才需要计算。
 			ans[i][j] = ans[i-1][j] + ans[i-1][j-1]
 		}
 	}
