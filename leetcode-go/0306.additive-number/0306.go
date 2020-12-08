8f9317372289ba87c7d01d33e429d55974f85e47
GaryLiu1996 <1099365856@qq.com>
2020-12-08 14:21:30 +0800

    update 0306.累加数

---
+++ leetcode-go/0306.additive-number/0306.go
@@ -0,0 +1,51 @@
+package _306_additive_number
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+//halforst 霜神代码
+//执行用时：
+//0 ms
+//, 在所有 Go 提交中击败了
+//100.00%
+//的用户
+//内存消耗：
+//1.9 MB
+//, 在所有 Go 提交中击败了
+//97.26%
+//的用户
+func isAdditiveNumber(num string) bool {
+	if len(num) < 3 {
+		return false
+	}
+	for firstEnd := 0; firstEnd < len(num)/2; firstEnd++ {
+		if num[0] == '0' && firstEnd > 0 {
+			break
+		}
+		first, _ := strconv.Atoi(num[0 : firstEnd+1])                                                                                    //string->int
+		for secondEnd := firstEnd + 1; int(math.Max(float64(firstEnd), float64(secondEnd-firstEnd))) < len(num)-secondEnd; secondEnd++ { //前两个数字的位数应该小于等于剩余数字位数
+			if num[firstEnd+1] == '0' && secondEnd-firstEnd > 1 {
+				break
+			}
+			second, _ := strconv.Atoi(num[firstEnd+1 : secondEnd+1])
+			if recursiveCheck(num, first, second, secondEnd+1) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+//Propagate for rest of the string
+func recursiveCheck(num string, x1 int, x2 int, left int) bool {
+	if left == len(num) {
+		return true
+	}
+	if strings.HasPrefix(num[left:], strconv.Itoa(x1+x2)) { //int->string
+		return recursiveCheck(num, x2, x1+x2, left+len(strconv.Itoa(x1+x2)))
+	}
+	return false
+}
---
+++ leetcode-go/0306.additive-number/0306_test.go
@@ -0,0 +1,10 @@
+package _306_additive_number
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_0306(t *testing.T) {
+	fmt.Println("--return--", isAdditiveNumber("199100199"))
+}
