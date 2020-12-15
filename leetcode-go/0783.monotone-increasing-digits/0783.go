1111d4ec4bb51e5c33edfc78e73b48f0eff82d9d
GaryLiu1996 <1099365856@qq.com>
2020-12-15 17:11:20 +0800

    update 0738. 单调递增的数字

---
+++ leetcode-go/0783.monotone-increasing-digits/0783.go
@@ -0,0 +1,14 @@
+package _783_monotone_increasing_digits
+
+func monotoneIncreasingDigits(N int) int {
+	low := 9
+	for i := 1; i <= N; i *= 10 {
+		value := (N / i) % 10
+		if value > low {
+			N = N/i*i - 1
+			value = (N / i) % 10
+		}
+		low = value
+	}
+	return N
+}
---
+++ leetcode-go/0783.monotone-increasing-digits/0783_test.go
@@ -0,0 +1,7 @@
+package _783_monotone_increasing_digits
+
+import "testing"
+
+func Test_0783(t *testing.T) {
+	monotoneIncreasingDigits(100)
+}
